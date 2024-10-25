6455355f272bd03ad2746b0f75fde5175866df13
fortisai <0xfortisai@gmail.com>
2024-10-25 18:13:26 +0200

    feat: wip

--- E.py
+++ E.py
@@ -1,9 +1,24 @@
+NEXT_PLACE = {
+    (2, 0): (0, 2),
+    (0, 2): (-2, 0),
+    (-2, 0): (0, -2),
+    (0, -2): (2, 0)
+}
+
+PLACE_TO_MOVE = {
+    (2, 0): (3, 0),
+    (0, 2): (0, 3),
+    (-2, 0): (-3, 0),
+    (0, -2): (0, -3)
+}
+
+
 def print_res(res, reversed_xy):
     if reversed_xy:
         for i in range(x):
             for j in range(y):
                 if res[j][i]:
-                    print('X', end='')
+                    print('x', end='')
                 else:
                     print('.', end='')
             print()
@@ -11,10 +26,53 @@ def print_res(res, reversed_xy):
         for i in range(y):
             for j in range(x):
                 if res[i][j]:
-                    print('X', end='')
+                    print('x', end='')
                 else:
                     print('.', end='')
             print()
+
+
+def can_place(res, n, m, x, y, dx, dy):
+    if x + dx < 0 or x + dx >= len(res[0]) or y + dy < 0 or y + dy >= len(res):
+        return False
+
+    for j in range(y, min(y + dy + 2, m)):
+        for i in range(x, (min(x + dx + 2, n))):
+            if res[j][i]:
+                return False
+
+    return True
+
+
+def place(res, n, m, x, y, dx, dy):
+    # place 2's
+    for i in range(x, x + dx + 1):
+        for j in range(y, y + dy + 1):
+            res[j][i] = 2
+    # place 1's (padding)
+    for i in range(max(0, x - 1), min(x + dx + 2, n)):
+        for j in range(max(0, y - 1), min(y + dy + 2, m)):
+            if res[j][i] == 0:
+                res[j][i] = 1
+    return res
+
+
+def solve_current_move(res, k, n, m, x, y, cur_dir, placed):
+    """Returns answer number and answer array"""
+    if n == 0:
+        return placed, res
+
+    place(res, x, y, cur_dir[0], cur_dir[1])
+
+    ptm = PLACE_TO_MOVE[cur_dir]
+
+    if can_place(res, n, m, x + ptm[0], y + ptm[1], cur_dir[0], cur_dir[1]):
+        # can place next the same
+        return solve_current_move(res, k - 1, n, m, x + ptm[0], y + ptm[1], cur_dir, placed + 1)
+    else:
+        # need to change direction
+        next_dir = NEXT_PLACE[cur_dir]
+        return solve_current_move(res, k - 1, n, m, x, y, next_dir, placed)
 
 
 for _ in range(int(input())):
@@ -23,28 +81,11 @@ for _ in range(int(input())):
     if y > x:
         x, y = y, x
         reversed_xy = True
-    res = [[False for _ in range(x)] for _ in range(y)]
-    
-    # Place desks horizontally first
-    for i in range(0, y, 2):
-        for j in range(0, x, 3):  # Changed from 4 to 3 since desks are now 1x2
-            if j + 1 > x - 1:  # Changed from j+2 to j+1 for 1x2 desks
-                break
-            if n > 0:
-                for k in range(2):  # Changed from 3 to 2 for desk size
-                    res[i][j + k] = True
-                n -= 1
-            else:
-                break
-                
-    # If there are remaining desks, place them vertically
-    if n > 0:
-        for i in range(0, y, 3):  # Changed spacing for vertical placement
-            if n > 0 and i + 1 < y:  # Changed from i+2 to i+1 for 1x2 desks
-                for k in range(2):  # Changed from 3 to 2 for desk size
-                    res[i + k][x - 1] = True
-                n -= 1
-            else:
-                break
-                
+    res = [[0 for _ in range(x)] for _ in range(y)]
+    ans, res = solve_current_move(res, n, x, y, 0, 0, (2, 0), 0)
+    if ans != n:
+        ans, res = solve_current_move(res, n, x, y, 1, 0, (0, 2), ans)
+    # if ans != n:
+    #     ans, res = solve_current_move(res, n, x + 1, y, (2, 0), ans)
+    print(ans)
     print_res(res, reversed_xy)
