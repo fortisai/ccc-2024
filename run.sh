b56313fdfd2bec6337bad57f66b6b01318d0a388
fortisai <0xfortisai@gmail.com>
2024-10-25 16:18:22 +0200

    feat: run script

---
+++ run.sh
@@ -0,0 +1,5 @@
+python E.py < level5_1.in > level5_1.out
+python E.py < level5_2.in > level5_2.out
+python E.py < level5_3.in > level5_3.out
+python E.py < level5_4.in > level5_4.out
+python E.py < level5_5.in > level5_5.out
